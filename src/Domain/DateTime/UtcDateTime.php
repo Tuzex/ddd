c2a8a745cf78cb8ad7f63fdca8e31e390883b25a
Edo Barnáš <iam@edobarnas.com>
2021-01-16 17:33:43 +0100

    Make UtcDateTime constructor final.

--- src/Domain/DateTime/UtcDateTime.php
+++ src/Domain/DateTime/UtcDateTime.php
@@ -9,7 +9,7 @@ use Tuzex\Ddd\Domain\TimeZone\UtcTimeZone;
 
 abstract class UtcDateTime implements DateTime
 {
-    public function __construct(
+    final private function __construct(
         private PointOfTime $pointOfTime,
         private UtcTimeZone $timeZone
     ) {}
@@ -27,15 +27,18 @@ abstract class UtcDateTime implements DateTime
     public function isPresent(Clock $clock): bool
     {
         //return $this->equals($clock->measure());
+        return true;
     }
 
     public function isFuture(Clock $clock): bool
     {
         //return $this->isLaterThan($clock->measure());
+        return true;
     }
 
     public function isPast(Clock $clock): bool
     {
         //return $this->isEarlierThan($clock->measure());
+        return true;
     }
 }
