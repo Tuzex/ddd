155552786c149825ddd05070cb8bc873056c79b2
Edo Barnas <iam@edobarnas.com>
2022-05-07 08:29:00 +0200

    Add the `UniversalId` doctrine type

---
+++ src/Infrastructure/Persistence/Doctrine/Dbal/Identifier/UniversalIdType.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Ddd\Infrastructure\Persistence\Doctrine\Dbal\Identifier;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\GuidType;
+use Tuzex\Ddd\Domain\Identifier\UniversalId;
+
+final class UniversalIdType extends GuidType
+{
+    public const NAME = 'tuzex.uid';
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+    {
+        return ($value instanceof UniversalId)
+            ? $value->value
+            : throw ConversionException::conversionFailed($value, self::NAME);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): UniversalId
+    {
+        if (! is_string($value) || ! preg_match('{^[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}$}Di', $value)) {
+            throw ConversionException::conversionFailed($value, self::NAME);
+        }
+
+        return new UniversalId($value);
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getMappedDatabaseTypes(AbstractPlatform $platform): array
+    {
+        return [self::NAME];
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+}
---
+++ tests/Infrastructure/Persistence/Doctrine/Dbal/Type/Identifier/UniversalIdTypeTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Ddd\Test\Infrastructure\Persistence\Doctrine\Dbal\Type\Identifier;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Tuzex\Ddd\Domain\Identifier\UniversalId;
+use Tuzex\Ddd\Infrastructure\Persistence\Doctrine\Dbal\Identifier\UniversalIdType;
+use Tuzex\Ddd\Test\Infrastructure\Persistence\Doctrine\Dbal\Type\TypeTest;
+
+final class UniversalIdTypeTest extends TypeTest
+{
+    private const UUID = 'a1506ff1-62f4-4377-a0c5-8baab8342219';
+
+    public function testItConvertsToDatabaseValue(): void
+    {
+        $universalId = new UniversalId(self::UUID);
+        $universalIdType = $this->getType();
+
+        $databaseUniversalId = $universalIdType->convertToDatabaseValue(
+            $universalId,
+            $this->mockPlatform()
+        );
+
+        $this->assertSame(self::UUID, $databaseUniversalId);
+    }
+
+    public function testItThrowsExceptionIfPhpValueIsInvalid(): void
+    {
+        $universalIdType = $this->getType();
+
+        $this->expectException(ConversionException::class);
+
+        $universalIdType->convertToDatabaseValue('non-uuid', $this->mockPlatform());
+    }
+
+    public function testItConvertsToValueObject(): void
+    {
+        $universalIdType = $this->getType();
+        $universalId = $universalIdType->convertToPHPValue(self::UUID, $this->mockPlatform());
+
+        $this->assertInstanceOf(UniversalId::class, $universalId);
+    }
+
+    public function testItThrowsExceptionIfDatabaseValueIsInvalid(): void
+    {
+        $universalIdType = $this->getType();
+
+        $this->expectException(ConversionException::class);
+
+        $universalIdType->convertToPHPValue('non-uuid', $this->mockPlatform());
+    }
+
+    protected function getType(): UniversalIdType
+    {
+        return new UniversalIdType();
+    }
+
+    protected function getTypeName(): string
+    {
+        return 'tuzex.uid';
+    }
+
+    protected function mockPlatform(): AbstractPlatform
+    {
+        return $this->createMock(AbstractPlatform::class);
+    }
+}
